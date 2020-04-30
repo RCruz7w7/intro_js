7f97e3b4076b4983b3f3ed35aa558b2072d40eab
RCruz7w7 <mokito15.0@outlook.com>
2020-04-30 11:53:28 -0400

    Prueba2

---
+++ 00_intro/prompt/prompt.js
@@ -0,0 +1,57 @@
+// Ejemplo #1 de prompt
+// let estudiante = {};
+
+// estudiante.nombre = prompt("Dime tu nombre");
+// estudiante.edad = prompt("Dime tu edad");
+// estudiante.curso = prompt("Dime tu curso");
+
+// console.log(estudiante.edad);
+
+// estudiante.edad = Number.parseInt(estudiante.edad);
+
+// console.log(estudiante.edad);
+
+
+// Ejemplo #2 de prompt
+// Declarar las variables
+// let añoActual = 2020;
+// let añoNacimiento = prompt("En que año naciste?");
+
+// Imprimir por la consola los valores ingresados
+// console.log(añoActual);
+// console.log(añoNacimiento);
+
+// Variable para hacer el calculo
+// let edadActual = añoActual - añoNacimiento;
+
+// Mostrar el resultado de su edad con el alert
+// alert(`Tu edad actual es ${edadActual} años de edad`);
+
+
+// Ejemplo #3 de prompt
+// let edad = prompt("Que edad tienes?");
+// let monto = Number.parseInt( promp("Que cantidad tienes en pesos") );
+
+// console.log(monto);
+// // monto = Number.parseInt(monto);
+// // convertir la edad en un numero
+// edad = Number.parseInt(edad);
+
+// let tasaDelDollar = 54.3;
+// let conversionEnDollar = monto / tasaDelDollar;
+
+// // console.log(`La conversacion es: ${conversionEnDollar.tofixed(2)}`);
+
+// console.log(`Haz digitado ${monto} pesos. La tasa del dollar es: ${tasaDelDollar}. Tu conversion es igual a: ${conversionEnDollar.toFixed(2)}`);
+
+
+// Ejemplo #4 de prompt
+// let numero1 = Number.parseInt( prompt("Digite un numero") );
+// let numero2 = Number.parseInt( prompt("Digite otro numero") );
+
+// // Sumar variables (numero1 y numero2)
+// let suma = numero1 + numero2;
+// console.log(numero1 + numero2); // Imprimiendo la suma en la consola
+
+// // Mostrar en una alerta el resultado final
+// alert(`La suma de ${numero1} y ${numero2} es igual a ${suma}. `);
